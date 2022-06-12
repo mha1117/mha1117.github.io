a414af647927e052de7f6e5916f65f07ef959905
mha1117 <oya22001@byui.edu>
2022-06-12 12:33:22 +0800

    initial commit

---
+++ lesson2/jsfile/current_date.js
@@ -0,0 +1,35 @@
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+  let today = new Date();
+  let dayName = days[today.getDay()];
+  let monthName = months[today.getMonth()];
+  let year = today.getFullYear();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  
+  let fullDate = dayName + ", " + monthName + " " + today.getDate() + ", " + year + " " + time;
+  
+  document.getElementById("currentDateAndTime").textContent = fullDate;
+  document.getElementById("currentYear").textContent = year;
