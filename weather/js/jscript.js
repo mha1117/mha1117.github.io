50d8d3697e54d3eb11dc9e35f40e0bb702fc9ec0
mha1117 <oya22001@byui.edu>
2022-06-25 10:13:26 +0800

    initial commit

--- weather/js/jscript.js
+++ weather/js/jscript.js
@@ -11,3 +11,11 @@ function toggleButton() {
         document.querySelector('#menuButton a').innerHTML = "&#127746; Menu";
     }
 }
+
+function displayBanner() {
+    dayOfWeek = new Date().getDay();
+    if (dayOfWeek == 6) {
+        document.querySelector('#banner').style.display = "block";
+    }
+    console.log(dayOfWeek);
+}
