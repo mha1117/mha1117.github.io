c4c37a71b568e671f9755f9fddcb4d20f94c807d
mha1117 <oya22001@byui.edu>
2022-06-26 15:02:53 +0800

    initial commit

--- weather/js/windChill.js
+++ weather/js/windChill.js
@@ -1,13 +1,6 @@
-function calcWindChill(t, s){
-    return 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
-}
+var temp= 41;
+var wSpeed= 5;
+var windChill= 35.74 + 0.6215 * temp - 35.75 * wSpeed ** 0.16 + 0.4275 * temp * wSpeed ** 0.16;
 
-function showWindChill(){
-    let speed = document.getElementById("windspeed").innerHTML;
-    let temp  = document.getElementById("high").innerHTML;
-    if (temp >= 50 && speed >= 3){
-        document.getElementById("windchill").innerHTML = Math.round(calcWindChill(temp, speed));
-    } else {
-        document.querySelector('windchill').innerHTML = "N/A";
-    }
-}
+var windChill= Math.round(windChill);
+document.getElementById("windchill").innerHTML= windChill;
