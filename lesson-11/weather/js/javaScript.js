0f73c40640899309aef4767232629904b7aa92f4
mha1117 <oya22001@byui.edu>
2022-07-16 14:10:46 +0800

    commit changes

--- lesson-11/weather/js/javaScript.js
+++
@@ -1,5 +0,0 @@
-function lastModified() {
-    let date = new Date(document.lastModified);  
-    dateString = date.toLocaleString('default', {weekday: 'long'}) + ', ' + date.getDate() + ' ' + date.toLocaleString('default', {month: 'long'}) + ' ' + date.getFullYear();
-    document.querySelector("#lastModified").innerHTML = "Last Updated: " + dateString;
-}
--- lesson-11/weather/js/windChill.js
+++
@@ -1,6 +0,0 @@
-var temp= 41;
-var wSpeed= 5;
-var windChill= 35.74 + 0.6215 * temp - 35.75 * wSpeed ** 0.16 + 0.4275 * temp * wSpeed ** 0.16;
-
-var windChill= Math.round(windChill);
-document.getElementById("windchill").innerHTML= windChill;
